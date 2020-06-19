921e4e4026bd631dc068c5784fa597c3edcec573
ashwinipandey86 <67057454+ashwinipandey86@users.noreply.github.com>
2020-06-19 17:03:11 +0530

    Update JDE9.2_RPM_INSTALL.sh

--- JDE9.2_RPM_INSTALL.sh
+++ JDE9.2_RPM_INSTALL.sh
@@ -1,6 +1,6 @@
 A=JDE9.2_RPM's
 
-cat ${filename} | while read line
+cat $A | while read line
 do
     tag=$(echo "${line}"|awk -F'=' '{print $1}')
     value=$(echo "${line}"|awk -F'=' '{print $2}')
