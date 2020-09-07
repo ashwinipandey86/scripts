86fa87be29cfff6fb4ebcbede1dc5e3615a5407f
ashwinipandey86 <67057454+ashwinipandey86@users.noreply.github.com>
2020-09-07 18:52:29 +0530

    Update ssh_conf.sh

--- ssh_conf.sh
+++ ssh_conf.sh
@@ -1,3 +1,5 @@
+ssh -i C:\Users\ashwinip5\.ssh\id_rsa.ppk opc@localhost
+sudo su - 
 cp /etc/ssh/sshd_config /etc/ssh/sshd_config.bkp
 sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
 service sshd restart
