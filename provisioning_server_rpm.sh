40556c560c00e3381f2984ac798a64ce0fec258e
ashwinipandey86 <67057454+ashwinipandey86@users.noreply.github.com>
2020-09-11 17:36:13 +0530

    Update provisioning_server_rpm.sh

--- provisioning_server_rpm.sh
+++ provisioning_server_rpm.sh
@@ -13,8 +13,7 @@ yum install -y nmap
 yum install -y ruby.x86_64
 yum install -y ruby-devel.x86_64
 yum install -y samba.x86_64
-yum install -y samba
-yum install -y client.x86_64
+yum install -y samba-client.x86_64
 yum install -y unzip.x86_64
 yum install -y zip.x86_64
 yum install -y zlib-devel.x86_64
