17095592a1f747573818db978cb1384b88343182
ashwinipandey86 <67057454+ashwinipandey86@users.noreply.github.com>
2020-09-11 17:35:39 +0530

    Update web_server_rpm.sh

--- web_server_rpm.sh
+++ web_server_rpm.sh
@@ -13,6 +13,6 @@ yum install -y net-tools
 yum install -y nmap
 yum install -y unzip.x86_64
 yum install -y zip.x86_64
-yum install -y zlibdevel.x86_64
+yum install -y zlib-devel.x86_64
 
 echo "RPM's Install Successfully"
