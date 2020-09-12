2c7ccf896ff2514ac4b52b79fb28186607feaa6f
ashwinipandey86 <67057454+ashwinipandey86@users.noreply.github.com>
2020-09-12 21:14:50 +0530

    Update web_server_rpm.sh

--- web_server_rpm.sh
+++ web_server_rpm.sh
@@ -7,12 +7,17 @@ echo "Installing RPM for Provisioning Server"
 yum install -y bind-utils
 yum install -y glibc.i686
 yum install -y glibc.x86_64
-yum install -y glibcdevel.x86_64
+yum install -y glibc-devel.x86_64
+yum install -y glibc-devel.i686
 yum install -y ksh.x86_64
 yum install -y net-tools
 yum install -y nmap
 yum install -y unzip.x86_64
 yum install -y zip.x86_64
 yum install -y zlib-devel.x86_64
+yum install -y zlib-devel.i686
+yum install -y zlib.i686
+yum install -y zlib.x86_64
+
 
 echo "RPM's Install Successfully"
