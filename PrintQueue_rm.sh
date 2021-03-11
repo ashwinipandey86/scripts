fcdfc2f9faa362de25e73065701c612eb7338c91
ashwinipandey86 <67057454+ashwinipandey86@users.noreply.github.com>
2021-03-11 16:43:40 +0530

    Create PrintQueue_rm.sh

---
+++ PrintQueue_rm.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "##################### Delete 180 Days Old PDF & CSV  file ###############"
+cd /app/jdedwardsppack/e920/PrintQueue
+current_path=`pwd`
+
+
+if [ "$current_path" = "/app/jdedwardsppack/e920/PrintQueue" ];
+   then
+        echo "Run below command to take output and delete above 180 days file"
+        find . -mtime +178 -name "*_PDF" > /tmp/print_queue_pdf.txt
+        find . -mtime +178 -name "*.csv" > /tmp/print_queue_csv.txt
+        find . -mtime +178 -name "*_PDF" -exec rm {} \;
+        find . -mtime +178 -name "*.csv" -exec rm {} \;
+    else
+        echo "Current path is invalid"
+fi
